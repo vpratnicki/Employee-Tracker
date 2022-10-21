bdf814166affa36e1f192299a1b061379bff9343
vpratnicki <vanessa@pratnicki.net>
2022-10-21 13:36:08 -0400

    Chore: added manager id to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -22,8 +22,9 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    -- manager_id INTEGER,
-    FOREIGN KEY (role.id) 
+    manager_id INTEGER,
+    FOREIGN Key (manager_id) REFERENCES employees(id),
+    FOREIGN KEY (role_id) 
     REFERENCES role(id) 
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -16,13 +16,13 @@ VALUES
   ('Legal Team Lead', 250000, 4),
   ('Lawyer', 190000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ('John', 'Doe', 1),
-  ('Mike', 'Chan', 2),
-  ('Ashley', 'Rodriguez', 3),
-  ('Kevin', 'Tupik', 4),
-  ('Kunal', 'Singh', 5),
-  ('Malia', 'Brown', 6),
-  ('Sarah', 'Lourd', 7),
-  ('Tom', 'Allen', 8);
+  ('John', 'Doe', 1, NULL),
+  ('Mike', 'Chan', 2, 1),
+  ('Ashley', 'Rodriguez', 3, NULL),
+  ('Kevin', 'Tupik', 4, 3),
+  ('Kunal', 'Singh', 5, NULL),
+  ('Malia', 'Brown', 6, 5),
+  ('Sarah', 'Lourd', 7, NULL),
+  ('Tom', 'Allen', 8, 7);
